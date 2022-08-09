4244c76e31cce048acea337d66cd646a04e02ae0
DavidO-crypto <21132833+DavidO-crypto@users.noreply.github.com>
2022-08-09 17:51:40 +0100

    Uploaded

--- Python/100DaysOfPython/Day020.py
+++ Python/100DaysOfPython/Day020.py
@@ -27,6 +27,7 @@ def keyboard():
     screen.onkey(key="Left", fun=snake.move_left)
     screen.onkey(key="Right", fun=snake.move_right)
     screen.onkey(key="q", fun=screen.bye)
+    screen.onkey(key="r", fun=snake.reset_snake)
 
 
 def game_walls(_snake):
@@ -38,24 +39,29 @@ def game_walls(_snake):
 
 
 keyboard()
-points.plus_one()
+points.update_points()
 is_game_on = True
 while is_game_on:
-    if game_walls(snake.snake):
-        is_game_on = False
-        print(f"GAME OVER!!! Total points: {points.points}. You hit the wall.")
-        break
 
+    if game_walls(snake.snake):
+        # is_game_on = False
+        # print(f"GAME OVER!!! Total points: {points.points}. You hit the wall.")
+        # break
+        snake.reset_snake()
+        points.reset_points()
     for n in snake.snake[1:]:
         if snake.head.distance(n) < 1:
-            is_game_on = False
-            print(f"GAME OVER!!! Total points: {points.points}. You hit yourself.")
-            break
+            # is_game_on = False
+            # print(f"GAME OVER!!! Total points: {points.points}. You hit yourself.")
+            # break
+            snake.reset_snake()
+            points.reset_points()
 
     if snake.head.distance(food) < 14:
         food.new_location()
         snake.add_new()
         points.plus_one()
+        points.update_points()
 
     screen.update()
     sleep(0.05)
--- Python/100DaysOfPython/Day020_Classes.py
+++ Python/100DaysOfPython/Day020_Classes.py
@@ -55,6 +55,14 @@ class Snake:
         new.speed(SPEED)
         self.snake.append(new)
 
+    def reset_snake(self):
+        for i in self.snake:
+            i.setposition(1000, 1000)
+        self.snake.clear()
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+
 
 class Food(Turtle):
 
@@ -65,13 +73,13 @@ class Food(Turtle):
         self.shapesize(stretch_wid=0.5, stretch_len=0.5)
         self.color("green")
         self.speed(0)
-        x = random.randint(-int((SCREEN_WIDTH-30) / 2), int((SCREEN_WIDTH-30) / 2))
-        y = random.randint(-int((SCREEN_HEIGHT-30) / 2), int((SCREEN_HEIGHT-30) / 2))
+        x = random.randint(-int((SCREEN_WIDTH - 30) / 2), int((SCREEN_WIDTH - 30) / 2))
+        y = random.randint(-int((SCREEN_HEIGHT - 30) / 2), int((SCREEN_HEIGHT - 30) / 2))
         self.setposition((x, y))
 
     def new_location(self):
-        x = random.randint(-int((SCREEN_WIDTH-30) / 2), int((SCREEN_WIDTH-30) / 2))
-        y = random.randint(-int((SCREEN_HEIGHT-30) / 2), int((SCREEN_HEIGHT-30) / 2))
+        x = random.randint(-int((SCREEN_WIDTH - 30) / 2), int((SCREEN_WIDTH - 30) / 2))
+        y = random.randint(-int((SCREEN_HEIGHT - 30) / 2), int((SCREEN_HEIGHT - 30) / 2))
         self.setposition((x, y))
 
 
@@ -79,15 +87,33 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.points = Turtle()
-        self.points = -1
+        with open("Day020_Scores.txt") as file:
+            self.high_score = int(file.read())
+
+        self.points = 0
         self.hideturtle()
 
     def plus_one(self):
         self.points += 1
+        self.update_points()
+
+    def update_points(self):
         self.reset()
         self.penup()
         self.hideturtle()
         self.goto(0, 250)
         self.color("white")
-        self.write(f"Points = {self.points} ", True, align="center", font=("Arial", 14, "normal"))
+        self.write(f"Points: {self.points}     HighScore: {self.high_score}", True, align="center",
+                   font=("Arial", 14, "normal"))
         self.hideturtle()
+
+    def reset_points(self):
+        if self.points > self.high_score:
+            self.high_score = self.points
+        self.points = 0
+        self.update_points()
+        self.highscore()
+
+    def highscore(self):
+        with open("Day020_Scores.txt", "w") as file:
+            file.write(str(self.high_score))
