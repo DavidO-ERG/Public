b168e37685968e709b91793e8699a0bd31b227f2
D₳vid OlivΣir₳ <21132833+DavidO-crypto@users.noreply.github.com>
2022-08-17 18:52:14 +0100

    Update Ergo-Mining-Calculator_GUI.py
    Moved "Intro" into README.MD

--- ERGO/Mininig_Calculator/Ergo-Mining-Calculator_GUI.py
+++ ERGO/Mininig_Calculator/Ergo-Mining-Calculator_GUI.py
@@ -1,14 +1,3 @@
-# -------------------------- INTRO ---------------------------- #
-# Simplistic mining calculator
-# Completely offline. The next versions will bring API connectors for price and network values.
-# Returns a Value (positive/negative) depending on the given values.
-#
-# Assume that the 'ReturnedValue' is equal to the inserted values for currency.
-# A file is stored in the same folder as the script called 'Ergo-Mining-Calculator.json'
-# This file will store the values for each day only (as 'day-index').
-# If you insert multiple values in the same day, it will only save the last inserted.
-# The 'miner-revenue' and 'miner-spending' are stored as last pair values in json file.
-#
 # ------------------------- CHANGELOG --------------------------- #
 # v0.1 - 12/08/2022
 #       Initial release: Simplistic Mining Calculator.
