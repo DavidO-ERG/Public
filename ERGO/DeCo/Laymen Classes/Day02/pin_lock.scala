3c2d2ad2f255cd0cf34090acf242cc1a2fde64e1
DavidO-ERG <21132833+DavidO-ERG@users.noreply.github.com>
2022-09-15 14:37:38 +0100

    Updated

--- ERGO/DeCo/Laymen Classes/Day02/pin_lock.scala
+++ ERGO/DeCo/Laymen Classes/Day02/pin_lock.scala
@@ -6,53 +6,69 @@ import scorex.crypto.hash.Blake2b256
 
 object pin_lock {
   def main(args: Array[String]): Unit = {
-
+  
+	println("#" * 50)
     println("Welcome to the Pin-Lock Example!")
-    println("-" * 50)
+    println("#" * 50)
 
-    println("Defining and Hashing PIN: ")
+	println()
+    println("-" * 50)
+	println("Defining and Hashing PIN: ")
     val hashedPin = Blake2b256(scala.io.StdIn.readLine().getBytes())
     //    val hashedPin = Blake2b256("123456789".getBytes())
-    println(hashedPin)
+    println(hashedPin.mkString)
     println("-" * 50)
 
-    val pinLockScript =
+	println()
+    println("-" * 50)
+    println("This is the Smart contract (or Guard Script)")
+    println("As defined above, the R4 start to get the hashed PIN that the user inputed and then get the stored on-chain hashed PIN and compare both")
+    println("If both are equal, the function returns True and the transactions is successfully added")
+	val pinLockScript =
       s"""
       sigmaProp(INPUTS(0).R4[Coll[Byte]].get == OUTPUTS(0).R4[Coll[Byte]].get)
     """.stripMargin
     val pinLockContract = ErgoScriptCompiler.compile(Map(), pinLockScript)
-
-    println("This is the Smart contract (or Guard Script)")
-    println("As defined above, the R4 start to get the hashed PIN that the user inputed and then get the stored on-chain hashed PIN and compare both")
-    println("If both are equal, the function returns True and the transactions is successfully added")
     println(pinLockContract)
     println("-" * 50)
 
+	println()
+    println("-" * 50)
     println("Creating an address")
     val contractAddress = Pay2SAddress(pinLockContract.ergoTree)
     println("Pin Lock Contract Address: " + contractAddress)
     println("-" * 50)
 
+	println()
+    println("-" * 50)
     println("Creating an test environment to test")
     val blockchainSim = newBlockChainSimulationScenario("PinLock Scenario")
     println(blockchainSim)
     println("-" * 50)
 
+	println()
+    println("-" * 50)
     println("Creating a test Buyer")
     val userParty = blockchainSim.newParty("buyer")
     println(userParty)
     println("-" * 50)
 
+	println()
+    println("-" * 50)
     println("Create variable for 'funds' to add to wallet (value in nanoErgs)")
     val userFunds = 100000000
     println(userFunds)
     println("-" * 50)
 
+	println()
+    println("-" * 50)
     println("Add funds to add to wallet in unspent boxes(value in nanoErgs)")
     userParty.generateUnspentBoxes(toSpend = userFunds)
     userParty.printUnspentAssets()
     println("-" * 50)
 
+	println()
+    println("-" * 50)
     println("Create a box to store the guard script depositing half of user funds to lock")
     val pinLockBox = Box(value = userFunds / 2,
       script = pinLockContract,
@@ -60,6 +76,8 @@ object pin_lock {
     println(pinLockBox)
     println("-" * 50)
 
+	println()
+    println("-" * 50)
     println("Create a transaction, locking the user funds")
     val depositTransaction = Transaction(
       inputs = userParty.selectUnspentBoxes(toSpend = userFunds),
@@ -70,6 +88,8 @@ object pin_lock {
     println(depositTransaction)
     println("-" * 50)
 
+	println()
+    println("-" * 50)
     println("Signing the TX")
     val depositTransactionSigned = userParty.wallet.sign(depositTransaction)
     blockchainSim.send(depositTransactionSigned)
@@ -77,6 +97,8 @@ object pin_lock {
     userParty.printUnspentAssets()
     println("-" * 50)
 
+	println()
+    println("-" * 50)
     println("Create a box to store the guard script, withdraw half of user funds and pay for Fees")
     val withdrawBox = Box(value = userFunds / 2 - MinTxFee,
       script = contract(userParty.wallet.getAddress.pubKey),
@@ -84,6 +106,8 @@ object pin_lock {
     println(withdrawBox)
     println("-" * 50)
 
+	println()
+    println("-" * 50)
     println("Create a transaction, unlocking the user funds")
     val withdrawTransaction = Transaction(
       inputs = List(depositTransactionSigned.outputs(0)),
@@ -94,14 +118,20 @@ object pin_lock {
     println(withdrawTransaction)
     println("-" * 50)
 
+	println()
+    println("-" * 50)
     println("Signing the TX")
     val withdrawTransactionSigned = userParty.wallet.sign(withdrawTransaction)
     blockchainSim.send(withdrawTransactionSigned)
     println("User funds, after TX")
     userParty.printUnspentAssets()
     println("-" * 50)
-    println("-" * 50)
+	
+	println()
+    println("#" * 50)
     println(s"The user final balance is the starting balance minus 2 TX fees (0.002 ERG == 2000000 nanoERG)")
     userParty.printUnspentAssets()
+    println("#" * 50)
+	println()
   }
 }
